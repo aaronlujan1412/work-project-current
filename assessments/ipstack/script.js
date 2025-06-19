e765f56368c3a89c8d1ee276097ea7a7a89838be
aaronlujan1412 <xarvon1412@gmail.com>
2025-06-19 10:06:55 -0600

    feat: add testing data

---
+++ assessments/ipstack/script.js
@@ -0,0 +1,41 @@
+const mockData = {
+  ip:"123.123.12.123",
+  country_name: "United States",
+  city: "Salt Lake",
+  zip: "84128"
+}
+
+let ip = "";
+let country = "";
+let city = "";
+let zip = "";
+
+const useMock = true;
+
+switch (useMock) {
+  case true:
+    ip = mockData.ip;
+    country = mockData.country_name;
+    city = mockData.city;
+    zip = mockData.zip;
+    break;
+  case false:
+    fetch("https://api.ipstack.com/172.83.7.250?access_key=6c05027224217364784994e52646e49d")
+      .then(response => response.json())
+      .then(data => {
+        ip = data.ip;
+        country = data.country_name;
+        city = data.city;
+        zip = data.zip;
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      })
+    break;
+}
+
+const divs = {
+  ip: document.getElementById("ip"),
+  location: document.getElementById("location"),
+  zip: document.getElementById("zip"),
+};
