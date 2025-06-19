92d5e7992a118a50094ef01ea38e38cb3c07bb90
aaronlujan1412 <xarvon1412@gmail.com>
2025-06-19 11:19:30 -0600

    feat: move functionality to a button for interactivity

--- assessments/ipstack/script.js
+++ assessments/ipstack/script.js
@@ -13,33 +13,56 @@ const divs = {
 
 const apiKey = "6c05027224217364784994e52646e49d";
 
-let ip = "";
-let country = "";
-let city = "";
-let zip = "";
+const userInfo = {
+  ip: "",
+  country: "",
+  city: "",
+  zip: ""
+}
+
+function getUserInformation() {
+  fetch(`https://api.ipstack.com/172.83.7.250?access_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      userInfo.ip = data.ip;
+      userInfo.country = data.country_name;
+      userInfo.city = data.city;
+      userInfo.zip = data.zip;
+    })
+    .catch(error => {
+      console.error("Error fetching data:", error);
+    })
+}
+
+function displayUserInfo() {
+  divs.ip.innerText = `Your IP address is: ${userInfo.ip}`;
+  divs.location.innerText = `Your location is ${userInfo.city}, ${userInfo.country}`;
+  divs.zip.innerText = `Your zip code is ${userInfo.zip}`;
+}
 
 const useMock = true;
 
+document.getElementById("getInfo").addEventListener("click", displayUserInfo)
+
 switch (useMock) {
   case true:
-    ip = mockData.ip;
-    country = mockData.country_name;
-    city = mockData.city;
-    zip = mockData.zip;
+    userInfo.ip = mockData.ip;
+    userInfo.country = mockData.country_name;
+    userInfo.city = mockData.city;
+    userInfo.zip = mockData.zip;
     break;
   case false:
-    fetch(`https://api.ipstack.com/172.83.7.250?access_key=${apiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        ip = data.ip;
-        country = data.country_name;
-        city = data.city;
-        zip = data.zip;
-      })
-      .catch(error => {
-        console.error("Error fetching data:", error);
-      })
+    getUserInformation();
+    // fetch(`https://api.ipstack.com/172.83.7.250?access_key=${apiKey}`)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     ip = data.ip;
+    //     country = data.country_name;
+    //     city = data.city;
+        // zip = data.zip;
+      // })
+      // .catch(error => {
+        // console.error("Error fetching data:", error);
+      // })
     break;
 }
-
-
